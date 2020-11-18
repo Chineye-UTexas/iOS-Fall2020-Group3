1b703ef9bf5a5f7ba8a68d563d72019173f18316
Marissa Jenkins <mrssjenkins099@gmail.com>
2020-11-17 22:49:35 -0600

    art & feed updates
    added made logo & got pictures in the feed

--- Made-App/Made-App/ViewControllers/FeedView/FeedTable/FeedTableViewCell.swift
+++ Made-App/Made-App/ViewControllers/FeedView/FeedTable/FeedTableViewCell.swift
@@ -6,15 +6,18 @@
 //
 
 import UIKit
+import Firebase
+import Foundation
 
 class FeedTableViewCell: UITableViewCell {
     
     @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var userProfilePicture: UIImageView!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var projectTitle: UILabel!
     @IBOutlet weak var projectReview: UILabel!
+    
+    let storage = Storage.storage()
     
     static let identifier = "FeedTableViewCell"
     static func nib() -> UINib{
@@ -32,10 +35,29 @@ class FeedTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    // provides spacing around the table cell
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    }
+    
     func configure(with model: Project) {
         self.usernameLabel.text = uniqueID
-//        self.userProfilePicture.image = UIImage(named: model.userProfilePicture)
-        self.postImageView.image = UIImage(named: model.images[0] as! String)
+        // set image
+        if model.images[0] as! String != "" {
+            // Create a reference from a Google Cloud Storage URI
+            let gsReference = storage.reference(forURL: model.images[0] as! String)
+            // Download in memory with a maximum allowed size of 5MB (5 * 1024 * 1024 bytes)
+            gsReference.getData(maxSize: 5 * 1024 * 1024) { data, error in
+              if let error = error {
+                // Uh-oh, an error occurred!
+                print(error.localizedDescription)
+              } else {
+                // Data for image path is returned
+                self.postImageView.image = UIImage(data: data!)
+              }
+            }
+        }
         self.projectTitle.text = model.title as String
         self.projectReview.text = model.description as String
     }
