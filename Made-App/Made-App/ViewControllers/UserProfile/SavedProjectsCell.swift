35dbebd662b644bb2f8af2901ab55eaaf42bbf74
Marissa Jenkins <mrssjenkins099@gmail.com>
2020-11-19 00:00:34 -0600

    updated ui stuff
    made a lot of things yello

---
+++ Made-App/Made-App/ViewControllers/UserProfile/SavedProjectsCell.swift
@@ -0,0 +1,16 @@
+//
+//  SavedProjectsCell.swift
+//  Made-App
+//
+//  Created by Marissa Jenkins on 11/18/20.
+//
+
+import Foundation
+import UIKit
+
+class SavedProjectsCell: UITableViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+}
--- Made-App/Made-App/ViewControllers/UserProfile/SavedProjectsViewController.swift
+++ Made-App/Made-App/ViewControllers/UserProfile/SavedProjectsViewController.swift
@@ -10,12 +10,12 @@ import Firebase
 import SDWebImage
 import Foundation
 
-class SavedProjectsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class SavedProjectsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var table: UITableView!
     @IBOutlet weak var savedProjectsHeader: UILabel!
     
-    var savedList: [String] = []
+    var savedList: [Int: [String]] = [0: ["image1", "knitted sweater"], 1: ["image2", "melted crayons"], 2: ["image3", "balloon ring"], 3: ["image4", "birthday cake"]]
     var models = [Project]()
     var imageCounter = 0
     let storage = Storage.storage()
@@ -25,8 +25,8 @@ class SavedProjectsViewController: UIViewController, UICollectionViewDataSource,
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        table.dataSource = self
+        table.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,122 +36,88 @@ class SavedProjectsViewController: UIViewController, UICollectionViewDataSource,
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        ref = Database.database().reference()
-        let id = uniqueID.split(separator: ".")
-
-        ref.child("users/\(id[0])/saved-projects").observeSingleEvent(of: .value, with: { (snapshot) in
-                for child in snapshot.children {
-                    // each child is a saved project
-                    let snap = child as! DataSnapshot
-                    let projectID = snap.key
-//                    let value = snap.value as! NSDictionary
-//                    let images = value["images"] as! NSArray
-//                    let imageURL = images[0] as! String
-//                    print("key = \(projectID)  value = \(value)")
-//                    print("imageURL = \(imageURL)")
-//                    if !self.savedList.contains(imageURL) {
-//                        self.savedList.append(imageURL)
-//                    }
-                    self.ref.child("posts/\(projectID)").observeSingleEvent(of: .value, with: { (innerSnapshot) in
-                      // Get user value
-                        let title = innerSnapshot.childSnapshot(forPath: "/project-title").value as! NSString
-                        let creationDate = innerSnapshot.childSnapshot(forPath: "/creationTime").value as! NSString
-                        let description = innerSnapshot.childSnapshot(forPath: "/description").value as! NSString
-                        let category = innerSnapshot.childSnapshot(forPath: "/category").value as! NSString
-                        let difficulty = innerSnapshot.childSnapshot(forPath: "/difficulty").value as! NSString
-                        let instructions = innerSnapshot.childSnapshot(forPath: "/instructions").value as! NSString
-                        let timeValue = innerSnapshot.childSnapshot(forPath: "/time").value as! NSString
-                        let images = innerSnapshot.childSnapshot(forPath: "/images").value as! NSArray
-                        let timeUnit = innerSnapshot.childSnapshot(forPath: "/timeUnit").value as! NSString
-                        
-                        self.models.append(Project(title: title, category: category, description: description, instructions: instructions, timeValue: timeValue, timeUnit: timeUnit, difficulty: difficulty, images: images, creationDate: creationDate, username: id[0] as NSString, reviews: [], firebaseProjectID: projectID))
-                      }) { (error) in
-                        print(error.localizedDescription)
-                    }
-//                    let title = snapshot.childSnapshot(forPath: "\(projectID)/project-title").value as! NSString
-//                    let creationDate = snapshot.childSnapshot(forPath: "\(projectID)/creationTime").value as! NSString
-//                    let description = snapshot.childSnapshot(forPath: "\(projectID)/description").value as! NSString
-//                    let category = snapshot.childSnapshot(forPath: "\(projectID)/category").value as! NSString
-//                    let difficulty = snapshot.childSnapshot(forPath: "\(projectID)/difficulty").value as! NSString
-//                    let instructions = snapshot.childSnapshot(forPath: "\(projectID)/instructions").value as! NSString
-//                    let timeValue = snapshot.childSnapshot(forPath: "\(projectID)/time").value as! NSString
-//                    let images = snapshot.childSnapshot(forPath: "\(projectID)/images").value as! NSArray
-//                    let timeUnit = snapshot.childSnapshot(forPath: "\(projectID)/timeUnit").value as! NSString
 //
-//                    self.models.append(Project(title: title, category: category, description: description, instructions: instructions, timeValue: timeValue, timeUnit: timeUnit, difficulty: difficulty, images: images, creationDate: creationDate, username: id[0] as NSString, reviews: [], firebaseProjectID: projectID))
-                    
-                    print("after getting database saved projects")
-                }
-            })
+//        ref = Database.database().reference()
+//        let id = uniqueID.split(separator: ".")
+//
+//        ref.child("users/\(id[0])/saved-projects").observeSingleEvent(of: .value, with: { (snapshot) in
+//                for child in snapshot.children {
+//                    // each child is a saved project
+//                    let snap = child as! DataSnapshot
+//                    let projectID = snap.key
+//                    self.ref.child("posts/\(projectID)").observeSingleEvent(of: .value, with: { (innerSnapshot) in
+//                      // Get user value
+//                        let title = innerSnapshot.childSnapshot(forPath: "/project-title").value as! NSString
+//                        let creationDate = innerSnapshot.childSnapshot(forPath: "/creationTime").value as! NSString
+//                        let description = innerSnapshot.childSnapshot(forPath: "/description").value as! NSString
+//                        let category = innerSnapshot.childSnapshot(forPath: "/category").value as! NSString
+//                        let difficulty = innerSnapshot.childSnapshot(forPath: "/difficulty").value as! NSString
+//                        let instructions = innerSnapshot.childSnapshot(forPath: "/instructions").value as! NSString
+//                        let timeValue = innerSnapshot.childSnapshot(forPath: "/time").value as! NSString
+//                        let images = innerSnapshot.childSnapshot(forPath: "/images").value as! NSArray
+//                        let timeUnit = innerSnapshot.childSnapshot(forPath: "/timeUnit").value as! NSString
+//
+//                        self.models.append(Project(title: title, category: category, description: description, instructions: instructions, timeValue: timeValue, timeUnit: timeUnit, difficulty: difficulty, images: images, creationDate: creationDate, username: id[0] as NSString, reviews: [], firebaseProjectID: projectID))
+//                      }) { (error) in
+//                        print(error.localizedDescription)
+//                    }
+//                    print("after getting database saved projects")
+//                }
+//            })
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        collectionView.reloadData()
-        if models.count == 0 {
+        table.reloadData()
+        if savedList.count == 0 {
             self.savedProjectsHeader.text = "No saved projects right now!"
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return models.count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedList.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "savedProjectsCellIdentifier", for: indexPath) as! MadeImageCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "savedProjectsCell", for: indexPath) as! SavedProjectsCell
         
-        // this could be cool customizable!
-        cell.backgroundColor = UIColor.black
-        
-        let model = models[indexPath.item]
-        let currImageName = (model.images)[0] as! String
-        
-        // set image
-        if currImageName != "" {
-            // Create a reference from a Google Cloud Storage URI
-            let gsReference = storage.reference(forURL: currImageName)
-            // Download in memory with a maximum allowed size of 5MB (5 * 1024 * 1024 bytes)
-            gsReference.getData(maxSize: 5 * 1024 * 1024) { data, error in
-              if let error = error {
-                // Uh-oh, an error occurred!
-                print(error.localizedDescription)
-              } else {
-                // Data for image path is returned
-                cell.image.image = UIImage(data: data!)
-              }
-            }
-        }
-        
+        let item = savedList[indexPath.row]
+        cell.title.text = item![1]
+        cell.photo.image = UIImage(named: item![0])
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        currItem = indexPath.item
-        performSegue(withIdentifier: "savedProjectsToSingleViewSegue", sender: indexPath.row)
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: "savedProjectsToSingleViewSegue", sender: indexPath.row)
+//    }
     
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        if segue.identifier == "savedProjectsToSingleViewSegue",
-        let nextVC = segue.destination as? SinglePostViewController
-     {
-         print("in explore feed segue to single view")
-         nextVC.delegate = self
-         let model = models[currItem]
-         nextVC.singlePost = model
-         nextVC.titleOfPost = model.title as String
-         nextVC.caption = model.description as String
-         nextVC.postName = model.username as String
-         nextVC.photoURL = (model.images)[0] as? String
-         nextVC.firebasePostID = model.firebaseProjectID
-         nextVC.projectInstructions = model.instructions as String
-     }
-    }
+//    func getCellPosition() -> Int {
+//        let indexPath = table.indexPathForSelectedRow
+//        return indexPath!.row
+//    }
+//
+//
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//        if segue.identifier == "savedProjectsToSingleViewSegue",
+//        let nextVC = segue.destination as? SinglePostViewController
+//     {
+//         print("in explore feed segue to single view")
+//         nextVC.delegate = self
+//         let row = getCellPosition()
+//         let model = models[row]
+//         nextVC.singlePost = model
+//         nextVC.titleOfPost = model.title as String
+//         nextVC.caption = model.description as String
+//         nextVC.postName = model.username as String
+//         nextVC.photoURL = (model.images)[0] as? String
+//         nextVC.firebasePostID = model.firebaseProjectID
+//         nextVC.projectInstructions = model.instructions as String
+//     }
+//    }
 
 }
